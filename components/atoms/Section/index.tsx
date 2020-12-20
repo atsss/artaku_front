bf4c7c273bd9493319bdba3da8dcb6c8625cbb24
atsss <atsushi0623itoh@gmail.com>
2020-12-20 02:18:49 -0600

    :+1: create Section

---
+++ components/atoms/Section/index.tsx
@@ -0,0 +1,18 @@
+import style from './style.module.sass'
+
+interface Props {
+  children: React.ReactNode
+  color?: string
+  image?: string
+  className?: string
+}
+
+export const Section: React.FC<Props> = ({ children, color, image, className }) => {
+  const backgroundStyle = { backgroundColor: color, backgroundImage: `url(${image})` }
+
+  return (
+    <section className={[style.container, className].join(' ')} style={backgroundStyle}>
+      <div className={style.content}>{children}</div>
+    </section>
+  )
+}

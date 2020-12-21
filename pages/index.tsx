34294d82cedf910aa87cd059ee5b2a6ccd3bdcf1
atsss <atsushi0623itoh@gmail.com>
2020-12-21 01:00:21 -0600

    :+1: create English lp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,199 +1,47 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { Footer } from '../components/organisms/Footer'
+import { Header } from '../components/organisms/Header'
+import { Hero } from '../components/organisms/Hero'
+import { TwoColumn } from '../components/molecules/TwoColumn'
 
-export const Home = (): JSX.Element => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+export const Home = (): JSX.Element => {
+  const router = useRouter()
+  const { query, locale, locales, defaultLocale } = router
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+  console.log(query, locale, locales,defaultLocale);
 
-      <p className="description">
-        Get started by editing <code>pages/index.tsx</code>
-      </p>
-
-      <button
-        onClick={() => {
-          window.alert('With typescript and Jest')
-        }}
-      >
-        Test Button
-      </button>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
-    </main>
-
-    <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      .logo {
-        height: 1em;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-  </div>
-)
+  return (
+    <>
+      <Head>
+        <title>Cocchi</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Hero />
+      <TwoColumn
+        src="/img/block01.jpg"
+        title="We will keep your countless art works instead of you"
+        description="All you have to do is call us! Then we’ll take your art works and keep them instead of you. The number of art works is unlimited. Whenever you want to store your art works, you can call. That’s it! You don’t have to be worried about where to store them."
+        isReversed
+        className="mt-16"
+      />
+      <TwoColumn
+        src="/img/block02.jpg"
+        title="We will make your portfolio with professional photos of your art works"
+        description="After we take your art works, we will take professional photos of them. Then we will make your personal page on Cocchi and upload them on it. You can use the personal page as your portfolio as well. You don’t have to spend time on making and updating your portfolio anymore!"
+        className="mt-16"
+      />
+      <TwoColumn
+        src="/img/block03.jpg"
+        title="You can sell your art works through Cocchi"
+        description="We can match your art works and art lovers in New York better with machine learning. Firstly we will rent your art works to the art lovers. Then if they like your art works, they buy them and we pay you for the art works. You don’t need galleries because you can sell your art works here!"
+        isReversed
+        className="mt-16 mb-16"
+      />
+      <Footer />
+    </>
+  )
+}
 
 export default Home

f0d10919f8be2d2034b3a90c6bb0071d604e6670
atsss <atsushi0623itoh@gmail.com>
2020-12-21 00:02:44 -0600

    :+1: create Footer

---
+++ components/organisms/Footer/index.tsx
@@ -0,0 +1,14 @@
+import style from './style.module.sass'
+import { Section } from '../../atoms/Section'
+import { Button } from '../../atoms/Button'
+
+export const Footer = (): JSX.Element => (
+  <footer>
+    <Section color="#F1F1F1">
+      <div className={style.block}>
+        <p className={style.text}>$200 per month</p>
+        <Button href="https://tailwindcss.com/docs/vertical-align" size="l" className="m-auto sm:mt-9 md:mt-12">Apply now</Button>
+      </div>
+    </Section>
+  </footer>
+)

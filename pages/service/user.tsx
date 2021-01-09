cc77825bbd01def93fd3f2e91dc0894f8b20b970
atsss <atsushi0623itoh@gmail.com>
2021-01-08 21:26:57 -0600

    :lipstick: change images for user lp

--- pages/service/user.tsx
+++ pages/service/user.tsx
@@ -21,20 +21,20 @@ export const User = (): JSX.Element => {
       <Header />
       <Hero />
       <TwoColumn
-        src="/img/block01.jpg"
+        src="/img/block04.jpg"
         title={i18n[locale].user.sections[0].title}
         description={i18n[locale].user.sections[0].description}
         isReversed
         className="mt-16"
       />
       <TwoColumn
-        src="/img/block02.jpg"
+        src="/img/block05.jpg"
         title={i18n[locale].user.sections[1].title}
         description={i18n[locale].user.sections[1].description}
         className="mt-16"
       />
       <TwoColumn
-        src="/img/block03.jpg"
+        src="/img/block06.jpg"
         title={i18n[locale].user.sections[2].title}
         description={i18n[locale].user.sections[2].description}
         isReversed

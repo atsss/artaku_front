226b214eae170db7f6d35fb1928abcfd4001a122
atsss <atsushi0623itoh@gmail.com>
2021-01-22 01:55:04 -0600

    :lipstick: fix artist/Layout component style

--- components/organisms/artist/Layout/index.tsx
+++ components/organisms/artist/Layout/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import style from './style.module.sass'
 
 interface Props {
   description?: string
@@ -20,17 +21,19 @@ export const Layout: React.FC<Props> = ({description, keywords, children}) => {
         { description && (<meta name="description" content={description} />) }
         { keywords && (<meta name="keywords" content={keywords.join(', ')} />) }
       </Head>
-      <aside>
-        <h1>{userId}</h1>
-        <nav>
-          <Link href={`/artist/${userId}`}>Home</Link>
-          <Link href={`/artist/${userId}/works`}>Works</Link>
-          <Link href={`/artist/${userId}/about`}>About</Link>
-          <Link href={`/artist/${userId}/events`}>Events</Link>
-          <Link href={`/artist/${userId}/contact`}>Contact</Link>
-        </nav>
-      </aside>
-      <main>{children}</main>
+      <div className="flex">
+        <aside className="w-1/4 p-6">
+          <h1>{userId}</h1>
+          <nav className="flex flex-col">
+            <Link href={`/artist/${userId}`}>Home</Link>
+            <Link href={`/artist/${userId}/works`}>Works</Link>
+            <Link href={`/artist/${userId}/about`}>About</Link>
+            <Link href={`/artist/${userId}/events`}>Events</Link>
+            <Link href={`/artist/${userId}/contact`}>Contact</Link>
+          </nav>
+        </aside>
+        <main className={[style.main, "w-3/4", "p-6"].join(' ')}>{children}</main>
+      </div>
     </>
   )
 }

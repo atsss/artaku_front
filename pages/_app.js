66637d6a310339373e34a34378ce4232817b36e5
atsss <atsushi0623itoh@gmail.com>
2021-02-04 01:11:00 -0600

    :sparkles: set up apolloClient

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import '../styles/globals.sass'
 import 'tailwindcss/tailwind.css'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../lib/apolloClient'
 import * as gatag from '../lib/gatag'
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter()
+  const apolloClient = useApollo(pageProps.initialApolloState)
 
   useEffect(() => {
     if (!gatag.existsGaId) return
@@ -17,5 +20,9 @@ export default function MyApp({ Component, pageProps }) {
     return () => router.events.off('routeChangeComplete', handleRouteChange)
   }, [router.events])
 
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }

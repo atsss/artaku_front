e308c902c0a2ceac612e6d0476131d2986f33df2
atsss <atsushi0623itoh@gmail.com>
2021-02-09 23:28:09 -0600

    :shirt: Lint

--- components/atoms/Youtube/index.tsx
+++ components/atoms/Youtube/index.tsx
@@ -4,9 +4,7 @@ interface Props {
 }
 
 export const Youtube: React.FC<Props> = ({ id, className }) => (
-  <div
-    className={className}
-  >
+  <div className={className}>
     <iframe
       className="w-full md:w-6/12 h-40 md:h-96"
       src={`https://www.youtube.com/embed/${id}?rel=0`}
--- components/organisms/artist/ProcessLists/index.tsx
+++ components/organisms/artist/ProcessLists/index.tsx
@@ -15,23 +15,27 @@ export const ProcessLists: React.FC<Props> = ({ processes }) => (
         <Txt tag="h3" font="sans">
           {pro.doneAt}
         </Txt>
-        {pro.summaryVideoId && <Youtube id={pro.summaryVideoId} className="mt-4"/>}
+        {pro.summaryVideoId && (
+          <Youtube id={pro.summaryVideoId} className="mt-4" />
+        )}
         {pro.descriptions &&
           pro.descriptions.map((description, index) => (
             <SubTxt key={index} size="s" className="mt-4">
               {description}
             </SubTxt>
           ))}
-        {pro.imageUrls && pro.imageUrls.map((imageUrl, index) => (
-          <div key={index} className="mt-8">
-            <Image src={imageUrl} width={800} height={500} />
-          </div>
-        ))}
-        {pro.videoIds && pro.videoIds.map((videoId, index) => (
-          <div key={index} className="mt-8">
-            <Youtube id={videoId}/>
-          </div>
-        ))}
+        {pro.imageUrls &&
+          pro.imageUrls.map((imageUrl, index) => (
+            <div key={index} className="mt-8">
+              <Image src={imageUrl} width={800} height={500} />
+            </div>
+          ))}
+        {pro.videoIds &&
+          pro.videoIds.map((videoId, index) => (
+            <div key={index} className="mt-8">
+              <Youtube id={videoId} />
+            </div>
+          ))}
       </Section>
     ))}
   </>

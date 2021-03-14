cc298bcd5f193e228439d3825285349a6fb6e5cf
atsss <atsushi0623itoh@gmail.com>
2021-03-14 14:04:48 -0400

    :lipstick: add artwork examples

--- components/organisms/lp/Artworks/index.tsx
+++ components/organisms/lp/Artworks/index.tsx
@@ -1,18 +1,30 @@
 import style from './style.module.sass'
+import Image from 'next/image'
 import { Section } from '../../../atoms/Section'
-import { Button } from '../../../atoms/Button'
+import { SubButton } from '../../../atoms/Button'
 
 export const Artworks = (): JSX.Element => (
   <Section isGray>
     <div className={style.block}>
-      <p className={style.text}>Our artwork </p>
-      <Button
+      <p className={style.text}>Our artworks</p>
+      <ul className={[style.lists, 'mt-10'].join(' ')}>
+        <li className={style.list}>
+          <Image src="/img/artwork01.jpg" width={500} height={500} />
+        </li>
+        <li className={style.list}>
+          <Image src="/img/artwork02.jpg" width={500} height={500} />
+        </li>
+        <li className={style.list}>
+          <Image src="/img/artwork03.jpg" width={500} height={500} />
+        </li>
+      </ul>
+      <SubButton
         href="https://www.instagram.com/artaku.art/"
         size="l"
         className="m-auto mt-10"
       >
         See all
-      </Button>
+      </SubButton>
     </div>
   </Section>
 )

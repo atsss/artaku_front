af1b0e54bfa55d9c70b2735c76ffd2be366472e3
atsss <atsushi0623itoh@gmail.com>
2021-03-22 20:47:33 -0400

    :bug: fix typo

--- components/organisms/lp/Concept/index.tsx
+++ components/organisms/lp/Concept/index.tsx
@@ -5,7 +5,7 @@ import { Youtube } from '../../../atoms/Youtube'
 export const Concept = (): JSX.Element => (
   <Section isGray isPadding>
     <Txt tag="h2" size="xl" weight="bold" font="sans" position="center">
-      Concept vido
+      Concept video
     </Txt>
     <Youtube id="eZPRo8yHKtk" className="mt-10 flex justify-center" />
   </Section>

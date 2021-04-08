6025334ab869ce5e13f400ac84294d5db6dfc39d
atsss <atsushi0623itoh@gmail.com>
2021-04-08 00:42:56 -0400

    :shower: remove unncessary links

--- components/organisms/artist/Navigation/index.tsx
+++ components/organisms/artist/Navigation/index.tsx
@@ -1,4 +1,3 @@
-// import style from './style.module.sass'
 import Link from 'next/link'
 import { Txt } from '../../../atoms/Txt'
 import { User } from '../../../../interfaces'
@@ -13,21 +12,12 @@ export const Navigation: React.FC<Props> = ({ user }) => (
       {user.name}
     </Txt>
     <nav className="flex md:flex-col md:mt-4 flex-row mt-2">
-      {/* <Txt size="s" font="sans" className="mt-2"> */}
-      {/*   <Link href={`/artist/${user.slug}`}>Home</Link> */}
-      {/* </Txt> */}
       <Txt size="s" font="sans" className="mt-2">
         <Link href={`/${user.slug}`}>About</Link>
       </Txt>
       <Txt size="s" font="sans" className="mt-2 ml-6 md:ml-0">
         <Link href={`/${user.slug}/works`}>Works</Link>
       </Txt>
-      {/* <Txt size="s" font="sans" className="mt-2"> */}
-      {/*   <Link href={`/artist/${user.slug}/events`}>Events</Link> */}
-      {/* </Txt> */}
-      {/* <Txt size="s" font="sans" className="mt-2"> */}
-      {/*   <Link href={`/artist/${user.slug}/contact`}>Contact</Link> */}
-      {/* </Txt> */}
     </nav>
   </>
 )
